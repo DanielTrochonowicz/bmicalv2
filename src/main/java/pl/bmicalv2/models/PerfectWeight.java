665140db19137a1766786112e8e1eb8297da5835
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-15 13:15:53 +0100

    bmi

---
+++ src/main/java/pl/bmicalv2/models/PerfectWeight.java
@@ -0,0 +1,38 @@
+package pl.bmicalv2.models;
+
+import java.text.DecimalFormat;
+
+public class PerfectWeight {
+
+    private double height;
+    private double weight;
+    DecimalFormat decimalFormat = new DecimalFormat();
+
+
+    public PerfectWeight(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getPerfectWeight(){
+        return "Idealna waga dla Twojego wzrostu to (" + height + "cm) wynośi: " +
+                decimalFormat.format((0.0001 * height * height * 18.5)) + "Kg - " +
+                decimalFormat.format((0.0001 * height * height * 25)) + "Kg";
+    }
+}
