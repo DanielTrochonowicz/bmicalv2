87ca5d1f56e6053240a4d20919cb2aa3faf3f1b5
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-15 13:22:02 +0100

    bmi update

---
+++ src/test/java/pl/bmicalv2/BmiCalcTest.java
@@ -0,0 +1,21 @@
+package pl.bmicalv2;
+
+
+import org.junit.Test;
+import pl.bmicalv2.models.BmiCalc;
+
+import static org.junit.Assert.assertEquals;
+
+public class BmiCalcTest {
+
+    @Test
+    public void testGetBmi(){
+        BmiCalc bmiCalc = new BmiCalc(85,178);
+        assertEquals(26.82742078020452,bmiCalc.getBmi(),0);
+
+        BmiCalc bmiCalc2 = new BmiCalc(65,165);
+        assertEquals(23.875114784205692,bmiCalc2.getBmi(),0);
+
+    }
+
+}
---
+++ src/test/java/pl/bmicalv2/Bmicalv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bmicalv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bmicalv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
